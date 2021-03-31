963e18db80b154843ee346c7aaf1ca5ef57ad5bc
Thales Testoni <thalestestoni@gmail.com>
2021-03-30 21:51:01 -0300

    handleToggleTaskCompletion and handleRemoveTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -32,11 +32,21 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id){
+        task.isComplete =  !task.isComplete;
+      }
+
+      return task;
+    });
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const updatedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(updatedTasks);
   }
 
   return (
